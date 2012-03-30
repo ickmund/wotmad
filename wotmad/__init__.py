802dfcb796ef826d8978143bf176f10c0aa92399
Alex Vidal <alex.vidal@gmail.com>
2012-03-30 17:38:50 -0500

    Logins work a bit better now.

--- wotmad/__init__.py
+++ wotmad/__init__.py
@@ -1,5 +1,8 @@
+from hashlib import md5
+import subprocess
+
 from flask import Flask
-from flask import g, request, render_template, jsonify
+from flask import g, request, render_template, jsonify, json, flash, url_for, redirect
 
 from flaskext import mongoengine
 from flaskext import login
@@ -43,6 +46,15 @@ class User(mongo.Document):
     def get_id(self):
         return unicode(self.id)
 
+    def __repr__(self):
+        return u'<User: {0}'.format(self.email)
+
+    def __unicode__(self):
+        return unicode(self.email)
+
+    def __str__(self):
+        return self.__unicode__()
+
     @classmethod
     def load_user(klass, id):
         return klass.objects.get(id=id)
@@ -69,10 +81,43 @@ def do_login():
     form = LoginForm()
     if form.validate_on_submit():
         assertion = form.data['assertion']
+        audience = request.url_root.rstrip('/')
         # Do the actual browserid login here
-        return "Assertion: {0}".format(assertion)
+        payload = "assertion={0}&audience={1}".format(assertion, audience)
+        print "Verifying with payload: {0}".format(payload)
+
+        curl = subprocess.Popen(['curl', '-d', payload,
+                                 'https://browserid.org/verify'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        try:
+            out, err = curl.communicate()
+            result = json.loads(out)
+        except ValueError:
+            return "Error connecting with BrowserID."
+
+        if result['status'] == 'failure':
+            return "Login failed. Reason: {reason}".format(**result)
+
+        email = result['email']
+
+        # Get or create the user
+        defaults = dict(username=md5(email).hexdigest(), active=False)
+        user, created = User.objects.get_or_create(email=email,
+                                                   defaults=defaults)
+
+        login.login_user(user, remember=True)
+
+        flash("Logged in.", 'success')
+        return redirect(url_for('index'))
 
     return jsonify(form.errors)
+
+
+@app.route('/accounts/logout/')
+def do_logout():
+    login.logout_user()
+    flash("Logged out", 'success')
+    return redirect(url_for('index'))
 
 
 @auth.user_loader
